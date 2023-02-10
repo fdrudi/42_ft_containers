35d6c2e551c642282facb07610336d262ff73e71
Mattia Cerchi <mcerchi@e4r6p2.42roma.it>
2023-02-10 18:04:18 +0100

    update_10/02_1

--- vector.hpp
+++ vector.hpp
@@ -16,6 +16,32 @@ namespace ft
 		typedef	const value_type&									const_reference;
 		typedef	typename allocator_type::pointer					pointer;
 		typedef	typename allocator_type::const_pointer				const_pointer;
-		typedef
+		typedef	ft::random_access_iterator<value_type>				iterator;
+		typedef	ft::random_access_iterator<const value_type>		const_iterator;
+		typedef	ft::reverse_iterator<iterator>						reverse_iterator;
+		typedef	ft::reverse_iterator<const_iterator>				const_reverse_iterator;
+
+		vector()
+		{};
+
+		// explicit vector( const Allocator& alloc );
+
+		// explicit vector( size_type count,
+        //          const T& value = T(),
+        //          const Allocator& alloc = Allocator());
+
+		// template< class InputIt >
+		// vector( InputIt first, InputIt last, const Allocator& alloc = Allocator() );
+
+		// vector( const vector& other );
+
+		private:
+
+		allocator_type	_alloc;
+		size_type		_count;
+		pointer			_begin;
+		pointer			_end;
+
+
 	};
 }
