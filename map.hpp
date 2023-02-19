b26cafded43190f2b1c24042331fc11e4dc41792
Francesco Drudi <fdrudi@e4r4p1.42roma.it>
2023-02-19 17:43:27 +0100

    Map started

---
+++ map.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <algorithm>
+#include "utility.hpp"
+#include "rb_tree.hpp"
+#include "iterator.hpp"
+
+namespace ft
+{
+	template< class Key, class T, class Compare = std::less<Key>, class Allocator = std::allocator<std::pair<const Key, T> > >
+	class map
+	{
+
+	};
+}
