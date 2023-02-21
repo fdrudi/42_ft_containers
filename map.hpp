8a6b4575cd55edd1948fe24f0fa77993fb012dbe
Fdrudi <drudi.francesco.99@gmail.com>
2023-02-21 16:46:06 +0100

    map

--- map.hpp
+++ map.hpp
@@ -8,8 +8,15 @@
 namespace ft
 {
 	template< class Key, class T, class Compare = std::less<Key>, class Allocator = std::allocator<std::pair<const Key, T> > >
-	class map
+	class map : public RBTree<ft::pair<const Key, T>, Node<ft::pair<const Key, T> >,  RBIterator<ft::pair<const Key, T>, Compare, Node<ft::pair<const Key, T> > >, RBIteratorConst<ft::pair<const Key, T>, Compare, Node<ft::pair<const Key, T> > >, Compare, Allocator>
 	{
+		public:
+
+		typedef	Key													key_type;
+		typedef	T													mapped_type;
+		typedef	pair<const key_type, mapped_type>					value_type;
+		typedef	Compare												key_compare;
+		typedef	
 
 	};
 }
